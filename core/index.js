cc04bb30524a201f9a05d62332cc374cbde7b889
TazyFoundSoup <gardendistrict0x0@outlook.com>
2026-04-17 16:28:23 +1000

    add indexes

---
+++ core/index.js
@@ -0,0 +1,8 @@
+export { thing } from "./thing.js"
+export { Frame } from "./frame.js"
+export { render } from "./render.js"
+
+export { TextInput } from "./inputs.js"
+export { PasswordInput } from "./inputs.js"
+export { FileInput } from "./inputs.js"
+export { TelephoneInput } from "./inputs.js"
